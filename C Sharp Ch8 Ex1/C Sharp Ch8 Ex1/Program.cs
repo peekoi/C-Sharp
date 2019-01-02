9582e7728ca3de09f537bb7356f3c65781d7895b
Peetu Koistinen <Peetu Koistinen@DESKTOP-37JESRN>
2019-01-02 12:09:43 +0200

    Ch8Ex1 Push

---
+++ C Sharp Ch8 Ex1/C Sharp Ch8 Ex1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C_Sharp_Ch8_Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
